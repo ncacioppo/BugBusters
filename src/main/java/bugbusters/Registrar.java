f43488124a4584fc50c45691a54f20b038062dec
ANM4388 <mealyan20@gcc.edu>
2024-03-16 19:09:31 -0400

    added Registrar constructor and (schema, username, password) to connectToDB()

--- src/main/java/bugbusters/Registrar.java
+++ src/main/java/bugbusters/Registrar.java
@@ -1,30 +1,37 @@
 package bugbusters;
 
 import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Properties;
-import java.util.Set;
 
 public class Registrar {
-    private static Set<String> majors;  //may want to use list indexing is more efficient
-    private static Set<String> minors;
-    private static List<Course> courses;
+    private static ArrayList<String> majors;  //may want to use list indexing if more efficient
+    private static ArrayList<String> minors;
+    private static ArrayList<Course> courses;
     private static Connection conn;
 
-    //TODO:we can make a Registrar constructor (+remove 'static' throughout) if for multiple universities
+    public Registrar() {
+        majors = new ArrayList<>();
+        minors = new ArrayList<>();
+        courses = new ArrayList<>();
+        conn = null;
+    }
 
-    private static boolean connectToDB() {
+    private static boolean connectToDB(String schema, String username, String password) {
         try {
             //Get a properties variable so that we can pass the username and password to
             // the database.
             Properties info = new Properties();
 
             //Set the username and password
-            info.put("user", "u222222");        //User: Andrea
-            info.put("password", "p222222");    //User: Andrea
+            info.put("user", username);
+            info.put("password", password);
+
 
             //Connect to the database (schemabugbuster)
             conn = DriverManager.getConnection("jdbc:mysql://CSDB1901/" + "schemaBugBuster", info);
+//            conn = DriverManager.getConnection("jdbc:mysql://CSDB1901/" + schema, info);
             //TODO: delete this print statement after testing
             System.out.println("Connection successful");
         } catch(SQLException e) {
@@ -58,25 +65,26 @@ public class Registrar {
      * combines ex. "B.S." + " in " + "Computer Science"
      * @return set of major names
      */
-    public static Set<String> getMajors() {
-        if(!connectToDB()) {
-            System.out.println("Unable to connect to database");
-            return null;
-        }
+    public static ArrayList<String> getMajors() {
+//        if(!connectToDB()) {
+//            System.out.println("Unable to connect to database");
+//            return null;
+//        }
+        majors = new ArrayList<String>();
 
         return majors;
     }
 
-    public static Set<String> getMinors() {
+    public static ArrayList<String> getMinors() {
         return minors;
     }
 
-    public static List<Course> getCourses() {
+    public static ArrayList<Course> getCourses() {
         return courses;
     }
 
     public static void main(String[] args) {
-        System.out.println(connectToDB());
+        System.out.println(connectToDB("schemaBugBuster","u222222","p222222"));
         System.out.println(disconnectFromDB());
     }
 }

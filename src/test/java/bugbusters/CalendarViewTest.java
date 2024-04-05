56b971222cfb3e7ec8d37e49a4a2bd8f6fd4e1c1
ncacioppo <ndcacioppo@gmail.com>
2024-04-04 21:13:20 -0400

    Added back CalendarView and CalendarViewTest. CalendarViewTest code is commented out because there is a missing function in ScheduleTest.

---
+++ src/test/java/bugbusters/CalendarViewTest.java
@@ -0,0 +1,20 @@
+package bugbusters;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalendarViewTest {
+
+    public static void main(String[] args) {
+        //Schedule schedule = ScheduleTest.getSampleSchedule();
+        //System.out.println(schedule.getName());
+        //CalendarView calendarView = new CalendarView(schedule);
+        //calendarView.printScheduleAsCalendar();
+    }
+}

1a5a85bc09dcf07784cfec6e1743a57b35a16398
DavidJPLeRoux <djpleroux@gmail.com>
2024-04-23 09:34:53 -0400

    Changed it to run console for now

--- src/main/java/bugbusters/Main.java
+++ src/main/java/bugbusters/Main.java
@@ -9,22 +9,24 @@ import javafx.stage.Stage;
 import java.io.File;
 import java.net.URL;
 
-public class Main extends Application {
-    @Override
-    public void start(Stage stage) throws Exception {
-        URL url = new File("src/main/java/bugbusters/UI/Login.fxml").toURI().toURL();
-        Parent root = FXMLLoader.load(url);
-        stage.setTitle("Grove City College Scheduler");
-        Scene scene = new Scene(root);
-//        String css = this.getClass().getResource("Applications.css").toExternalForm();
-//        scene.getStylesheets().add(css);
-        stage.setScene(scene);
-        stage.setMaximized(true);
-        stage.show();
-    }
+import static bugbusters.Run.run;
+
+public class Main /**extends Application**/ {
+//    @Override
+//    public void start(Stage stage) throws Exception {
+//        URL url = new File("src/main/java/bugbusters/UI/Login.fxml").toURI().toURL();
+//        Parent root = FXMLLoader.load(url);
+//        stage.setTitle("Grove City College Scheduler");
+//        Scene scene = new Scene(root);
+////        String css = this.getClass().getResource("Applications.css").toExternalForm();
+////        scene.getStylesheets().add(css);
+//        stage.setScene(scene);
+//        stage.setMaximized(true);
+//        stage.show();
+//    }
     public static void main(String[] args) {
-//        run();
-        launch();
+        run();
+//        launch();
     }
 
 }
